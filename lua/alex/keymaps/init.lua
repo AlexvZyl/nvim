c08f8cb68ab9b3d2df5bbc3bc72e3e6eaecfd496
AlexvZyl <alexandervanzyl@protonmail.com>
2023-07-31 13:50:07 +0200

    Using neovim functions for vscode

--- lua/alex/keymaps/init.lua
+++ lua/alex/keymaps/init.lua
@@ -67,6 +67,7 @@ function M.editing()
     keymap(n, '<C-a>', 'gg0vG$', default_settings)
     keymap(ex_i, 'dd', '"_dd', default_settings)
     keymap(ex_i, 'dw', '"_dw', default_settings)
+    keymap(ex_i, 'di', '"_di', default_settings)
     keymap(ex_i, 'd', '"_d', default_settings)
 end
 

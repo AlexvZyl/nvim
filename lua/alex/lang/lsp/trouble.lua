f3c8f2698b3492dd27cb6926c610d9f25429601f
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-09-17 11:31:44 +0200

    Use lua api to create autocmd

--- lua/alex/lang/lsp/trouble.lua
+++ lua/alex/lang/lsp/trouble.lua
@@ -3,10 +3,12 @@ local U = require 'alex.utils'
 require('trouble').setup {
     padding = true,
     height = 11,
-    use_diagnostic_signs = false,
     position = 'bottom',
+    use_diagnostic_signs = true,
     signs = U.diagnostic_signs,
     auto_preview = false,
 }
 
-vim.cmd [[ autocmd BufEnter * TroubleRefresh ]]
+vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+    command = 'TroubleRefresh'
+})

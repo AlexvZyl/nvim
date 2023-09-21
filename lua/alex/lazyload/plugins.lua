5cde582744cfec9cdf4fdc7ff41d0ddd162d4255
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-09-21 11:46:57 +0200

    WIP: Use custom go icons

--- lua/alex/lazyload/plugins.lua
+++ lua/alex/lazyload/plugins.lua
@@ -8,6 +8,12 @@ return {
         config = function() require 'alex.ui.dashboard' end,
     },
     {
+        -- Required by other packages
+        'nvim-tree/nvim-web-devicons',
+        config = function() require 'alex.ui.nvim-web-devicons' end,
+        lazy = true
+    },
+    {
         'NvChad/nvim-colorizer.lua',
         event = { 'User NvimStartupDone' },
         config = function() require 'alex.ui.colorizer' end,
---
+++ lua/alex/ui/nvim-web-devicons.lua
@@ -0,0 +1,13 @@
+local icons = require 'nvim-web-devicons'
+
+local get_icon = icons.get_icon
+local go_icon = get_icon('', '.go')
+
+-- Why is this not working???
+ icons.setup {
+    strict = true,
+    override_by_filename = {
+        ['go.mod'] = { icon = "" },
+        ['go.sum'] = { icon = "" },
+    }
+}

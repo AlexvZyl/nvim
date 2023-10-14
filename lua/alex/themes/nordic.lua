fe08a98fb9b4d99481c721acad91c45406267b89
AlexvZyl <alexandervanzyl@protonmail.com>
2023-10-14 16:07:27 +0200

    Nordic: Use more defaults

--- lua/alex/themes/nordic.lua
+++ lua/alex/themes/nordic.lua
@@ -1,10 +1,6 @@
 require('nordic').load {
-    reduced_blue = true,
     cursorline = {
         theme = 'dark',
-        bold = false,
-        bold_number = true,
-        blend = 0.7,
     },
 }
 

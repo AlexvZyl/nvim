d496ac8d32c42175137b5f81a2a909106b7e8a8e
AlexvZyl <alexandervanzyl@protonmail.com>
2023-12-28 11:14:13 +0200

    Ref: Format

--- lua/alex/lazyload/bootstrap.lua
+++ lua/alex/lazyload/bootstrap.lua
@@ -1,4 +1,5 @@
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system {
         'git',
@@ -9,4 +10,5 @@ if not vim.loop.fs_stat(lazypath) then
         lazypath,
     }
 end
+
 vim.opt.rtp:prepend(lazypath)
--- lua/alex/lazyload/init.lua
+++ lua/alex/lazyload/init.lua
@@ -1,26 +1,6 @@
 require 'alex.lazyload.bootstrap'
 require 'alex.lazyload.events'
 
--- Load events
-vim.api.nvim_create_autocmd('User', {
-    callback = function(_)
-        vim.api.nvim_exec_autocmds('User', {
-            pattern = 'NvimStartupDone',
-        })
-    end,
-    pattern = 'DashboardLoaded',
-    once = true,
-})
--- This will no longer work if Dashboard starts to set the value
-vim.api.nvim_create_autocmd('BufModifiedSet', {
-    callback = function(_)
-        vim.api.nvim_exec_autocmds('User', {
-            pattern = 'NvimStartupDone',
-        })
-    end,
-    once = true,
-})
-
 -- Load plugins
 local U = require 'alex.utils'
 local plugins = require 'alex.lazyload.plugins'

75b84ce9bf1d3dbbb66935418c3a5ca49ffa338b
AlexvZyl <alexandervanzyl@protonmail.com>
2024-01-08 10:23:35 +0200

    Ref(tokyonght): Improve telescope colors

--- lua/alex/themes/tokyonight.lua
+++ lua/alex/themes/tokyonight.lua
@@ -1,36 +1,54 @@
 local TC = require 'tokyonight.colors'
+local blend = require 'tokyonight.util'.blend
 
 local function on_highlights(H, C)
+    -- Native
+    H.MsgArea = { bg = C.bg_dark, fg = C.fg_dark }
+    H.SpecialCmpBorder = { bg = C.bg }
+    H.Pmenu = { bg = C.bg }
+
+    -- Tree
     H.NvimTreeNormal = { bg = C.bg }
     H.NvimTreeNormalNC = { bg = C.bg }
     H.NvimTreeWinSeparator = { fg = C.bg_dark, bg = C.bg }
+
+    -- Dashboard
     H.DashboardMruTitle = { fg = C.orange, bold = true }
     H.DashboardProjectTitle = { fg = C.orange, bold = true }
     H.DashboardProjectIcon = { fg = C.magenta, bold = true }
-    H.SagaNormal = { bg = C.bg }
-    H.SagaBorder = { bg = C.bg, fg = C.orange }
-    H.SpecialCmpBorder = { bg = C.bg }
-    H.Pmenu = { bg = C.bg }
+
+    -- Leap
     H.LeapBackdrop = {}
     H.LeapLabelPrimary = { bg = C.bg_dark, fg = C.red1 }
     H.LeapLabelSecondary = { bg = C.bg_dark, fg = C.red1 }
+
+    -- Indent blankline
     H.IndentBlanklineContextChar = { fg = C.bg_highlight }
     H.IndentBlanklineChar = { fg = C.bg_highlight }
-    H.TelescopePromptTitle = { fg = C.bg_dark, bg = C.orange }
-    H.TelescopePreviewTitle = { fg = C.bg_dark, bg = C.orange }
-    H.MsgArea = { bg = C.bg_dark, fg = C.fg_dark }
 
+    -- LSP Saga
     H.LspSagaHoverBorder = { fg = C.fg }
     H.SagaBorder = { fg = C.fg }
+    H.SagaNormal = { bg = C.bg }
+    H.SagaBorder = { bg = C.bg, fg = C.orange }
 
-    --H.TelescopeBorder = { fg = C.fg_dark, bg = C.bg_dark }
-    H.TelescopeBorder = { fg = TC.default.bg, bg = C.bg_dark }
-    --H.TelescopePromptBorder = { fg = C.fg_dark, bg = C.bg }
-    H.TelescopePromptBorder = { fg = TC.default.bg, bg = C.bg }
+    -- Telescope
+    H.TelescopePromptTitle = { fg = C.bg_dark, bg = C.orange }
+    H.TelescopePreviewTitle = { fg = C.bg_dark, bg = C.orange }
 
-    H.TelescopePromptNormal = { bg = C.bg }
+    --local prompt = blend(C.bg_dark, C.bg, 0.8)
+    local prompt = TC.default.bg_dark
+    H.TelescopePromptBorder = { fg = C.bg_dark, bg = prompt }
+    H.TelescopePromptNormal = { bg = prompt }
+
+    local preview = blend(C.bg_dark, C.bg, 0.15)
+    H.TelescopeNormal = { bg = preview }
+    H.TelescopeBorder = { fg = C.bg_dark, bg = preview }
+
+    -- Noice
     H.NoiceCmdline = { bg = C.bg_dark }
 
+    -- Copilot
     H.CopilotSuggestion = { fg = C.fg_gutter, italic = false }
 end
 

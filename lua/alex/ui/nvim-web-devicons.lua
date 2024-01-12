a76086ed68336114757d1b2206cd47e17e6ccd93
AlexvZyl <alexandervanzyl@protonmail.com>
2024-01-12 19:42:15 +0200

    Fix: Override go icons

--- lua/alex/ui/nvim-web-devicons.lua
+++ lua/alex/ui/nvim-web-devicons.lua
@@ -1,13 +1,19 @@
 local icons = require 'nvim-web-devicons'
 
-local get_icon = icons.get_icon
-local go_icon = get_icon('', '.go')
+--local get_icon = icons.get_icon_color
+--local go_icon, go_color = get_icon('main.go', 'go')
+
+local go_icon = {
+     icon = '',
+    color = "#519aba",
+    name = "go"
+}
 
 -- Why is this not working???
 icons.setup {
     strict = true,
     override_by_filename = {
-        ['go.mod'] = { icon = '' },
-        ['go.sum'] = { icon = '' },
+        ['go.mod'] = go_icon,
+        ['go.sum'] = go_icon,
     },
 }

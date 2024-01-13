54f80d584bac773845b5feededca61c8f512ecc3
AlexvZyl <alexandervanzyl@protonmail.com>
2024-01-13 16:19:29 +0200

    Feat: Neovide config

--- lua/alex/init.lua
+++ lua/alex/init.lua
@@ -3,6 +3,10 @@ if vim.g.vscode then
     return
 end
 
+if vim.g.neovide then
+    require 'alex.neovide'
+end
+
 -- This makes neovim load faster
 vim.loader.enable()
 
---
+++ lua/alex/neovide.lua
@@ -0,0 +1,11 @@
+-- Font
+vim.o.guifont = "JetBrainsMono NF:h8.8"
+
+-- Transparency
+vim.g.neovide_transparency = 0.98
+
+-- Shadows
+vim.g.neovide_floating_shadow = true
+vim.g.neovide_floating_z_height = 10
+vim.g.neovide_light_angle_degrees = 45
+vim.g.neovide_light_radius = 5

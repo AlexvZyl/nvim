42363eda68717daf5fa8f7fe6c6fca0ce0ffd70b
AlexvZyl <alexandervanzyl@protonmail.com>
2024-01-15 17:22:49 +0200

    Neovide: Add fps

--- lua/alex/neovide.lua
+++ lua/alex/neovide.lua
@@ -9,3 +9,7 @@ vim.g.neovide_floating_shadow = true
 vim.g.neovide_floating_z_height = 10
 vim.g.neovide_light_angle_degrees = 45
 vim.g.neovide_light_radius = 5
+
+
+-- Other
+vim.g.neovide_refresh_rate = 75

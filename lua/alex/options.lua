85c07b2101f41cb720825f00f04558cb04fea5de
AlexvZyl <alexandervanzyl@protonmail.com>
2024-04-14 14:06:47 +0200

    Ref: Move statuscolumn to options (it is not a plugin config)

--- lua/alex/options.lua
+++ lua/alex/options.lua
@@ -68,3 +68,30 @@ vim.cmd [[
 
 -- Enable blinking for wezterm
 vim.opt.guicursor = 'i:ver20-blinkon1,a:blinkon1'
+
+
+-- Statuscolumn was added in 0.9.
+if vim.version.major == 0 and vim.version.minor < 9 then return end
+
+--No separator.
+--vim.opt.numberwidth = 4
+--vim.opt.statuscolumn = "%= %{v:virtnum < 1 ? (v:relnum ? v:relnum : v:lnum) : ''}%=%s"
+
+-- With sepaartor.
+--vim.opt.numberwidth = 6
+--vim.opt.statuscolumn = "%= %{v:virtnum < 1 ? (v:relnum ? v:relnum : v:lnum) : ''}%=%s▎ "
+
+-- Signs first, right aligned relative number
+vim.opt.numberwidth = 5
+vim.opt.statuscolumn = ' %s%=%{v:relnum?v:relnum:v:lnum}%#WinSeparator#  '
+--No separator.
+--vim.opt.numberwidth = 4
+--vim.opt.statuscolumn = "%= %{v:virtnum < 1 ? (v:relnum ? v:relnum : v:lnum) : ''}%=%s"
+
+-- With sepaartor.
+--vim.opt.numberwidth = 6
+--vim.opt.statuscolumn = "%= %{v:virtnum < 1 ? (v:relnum ? v:relnum : v:lnum) : ''}%=%s▎ "
+
+-- Signs first, right aligned relative number
+vim.opt.numberwidth = 5
+vim.opt.statuscolumn = ' %s%=%{v:relnum?v:relnum:v:lnum}%#WinSeparator#  '

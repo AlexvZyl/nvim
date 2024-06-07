5b030109d2890b6a60699da1434d111f057a8402
AlexvZyl <alexandervanzyl@protonmail.com>
2024-06-07 10:58:43 +0200

    Netrw options

--- lua/alex/native/init.lua
+++ lua/alex/native/init.lua
@@ -1,3 +1,4 @@
 require("alex.native.general")
 require("alex.native.statuscolumn")
 require("alex.native.winbar")
+require("alex.native.netrw")
---
+++ lua/alex/native/netrw.lua
@@ -0,0 +1,4 @@
+vim.g.netrw_banner = 0
+vim.g.netrw_browse_split = 4
+vim.g.netrw_altv = 1
+vim.g.netrw_liststyle = 3

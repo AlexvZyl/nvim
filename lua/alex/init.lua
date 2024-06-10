c0110d7fbb2fa336a001772a6e3b62d13efab26a
AlexvZyl <alexandervanzyl@protonmail.com>
2024-06-10 11:10:51 +0200

    Move managers to plugins dir

--- lua/alex/init.lua
+++ lua/alex/init.lua
@@ -3,5 +3,5 @@
 if not require("alex.environments").should_setup() then return end
 
 require("alex.native")
-require("alex.managers.lazy")
+require("alex.plugins.managers.lazy")
 require("alex.keymaps").init()

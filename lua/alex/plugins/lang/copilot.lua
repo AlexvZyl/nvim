f39c29d354d201a594fc2ce7a606b5e45f293dd2
AlexvZyl <alexandervanzyl@protonmail.com>
2024-06-24 12:25:00 +0200

    Copilot has to be manuallyy enabled, no need to disable

--- lua/alex/plugins/lang/copilot.lua
+++ lua/alex/plugins/lang/copilot.lua
@@ -15,5 +15,3 @@ cmp.event:on("menu_opened", function() vim.b.copilot_suggestion_hidden = true en
 cmp.event:on("menu_closed", function() vim.b.copilot_suggestion_hidden = false end)
 
 require("alex.keymaps").copilot()
-
-vim.cmd("Copilot disable")

5d84aeec2bb999155af5f6899671da4c680c2359
AlexvZyl <alexandervanzyl@protonmail.com>
2024-10-01 16:57:39 +0200

    Tweak noice and oil

--- lua/alex/plugins/ui/oil-nvim.lua
+++ lua/alex/plugins/ui/oil-nvim.lua
@@ -1,8 +1,23 @@
 require("oil").setup({
     delete_to_trash = true,
-    view_options = { show_hidden = true },
+    view_options = {
+        show_hidden = true,
+        is_hidden_file = function(name, bufnr)
+            return false
+        end,
+        is_always_hidden = function(name, bufnr)
+            return false
+        end,
+    },
     watch_for_changes = true,
     default_file_explorer = true,
 })
 
 require("alex.keymaps").oil()
+
+
+require("oil.view").set_is_hidden_file(
+    function(name, buffer)
+        return false
+    end
+)

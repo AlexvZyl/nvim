723caacc6f8b7b7bc28e1ccddffac153c282133f
AlexvZyl <alexandervanzyl@protonmail.com>
2025-01-12 19:02:10 +0200

    Fix cursor blinking

--- lua/alex/environments/wezterm.lua
+++ lua/alex/environments/wezterm.lua
@@ -1,2 +1,7 @@
 -- Enable blinking cursor for wezterm's animation.
-vim.opt.guicursor = "i:ver20-blinkon1,a:blinkon1"
+-- TODO: Double check
+vim.opt.guicursor = {
+    'n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
+    'i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
+    'r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100'
+}

fa68d3ef1c31083eb3445dec1a184b67305fdd99
AlexvZyl <alexandervanzyl@protonmail.com>
2026-04-14 14:49:14 +0200

    Improve oil winbar handling

--- lua/alex/plugins/oil-nvim.lua
+++ lua/alex/plugins/oil-nvim.lua
@@ -9,3 +9,10 @@ require("alex.keymaps").oil()
 require("oil.view").set_is_hidden_file(function(_, _)
     return false
 end)
+
+vim.api.nvim_create_autocmd("User", {
+    pattern = "OilEnter",
+    callback = function()
+        require("alex.native.winbar").set_winbar()
+    end,
+})

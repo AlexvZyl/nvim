4584b59d31174b04ee87d44e356829fb1882f06f
AlexvZyl <alexandervanzyl@protonmail.com>
2026-05-08 17:12:54 +0200

    Fix statuscolumn

--- lua/alex/native/dashboard.lua
+++ lua/alex/native/dashboard.lua
@@ -1,5 +1,15 @@
 if vim.fn.argc() == 0 then
+    local startup_win = vim.api.nvim_get_current_win()
+
     require("lazy").show()
+
+    vim.schedule(function()
+        if vim.api.nvim_win_is_valid(startup_win) then
+            vim.api.nvim_win_call(startup_win, function()
+                require("alex.native.statuscolumn").default()
+            end)
+        end
+    end)
 end
 
 return {}
